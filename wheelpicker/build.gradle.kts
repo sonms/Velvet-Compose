56849722ca414aee2b1ca3c7d32bd5c74f3d18a1
sonms <smsms5676@naver.com>
2026-04-05 20:37:26 +0900

    [Add] material3 and kotlinx-collections-immutable to wheelpicker module

--- wheelpicker/build.gradle.kts
+++ wheelpicker/build.gradle.kts
@@ -35,6 +35,8 @@ android {
 
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.kotlinx.collections.immutable)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
