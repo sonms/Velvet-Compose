84798bf82b415b98d87f2f69bb2a73625b107e3a
sonms <smsms5676@naver.com>
2026-04-05 22:36:46 +0900

    [Add] Maven Central publishing configuration and Dokka plugin support

--- wheelpicker/build.gradle.kts
+++ wheelpicker/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.vanniktech)
+    alias(libs.plugins.dokka)
 }
 
 android {
@@ -44,4 +46,44 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+val velvetVersion = "0.0.1"
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.sonms",
+        artifactId = "wheelpicker",
+        version = velvetVersion
+    )
+
+    pom {
+        name.set("VelvetCompose WheelPicker")
+        description.set("Bring the classic iOS 3D wheel picker to your Jetpack Compose app\uD83D\uDE80 Features infinite scrolling and dynamic 3D graphic layers.")
+        url.set("https://github.com/sonms/Velvet-Compose")
+
+        licenses {
+            license {
+                name.set("Apache License 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("sonms")
+                name.set("sonms")
+                email.set("smsms5676@naver.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/sonms/Velvet-Compose")
+            connection.set("scm:git:git://github.com/sonms/Velvet-Compose.git")
+            developerConnection.set("scm:git:ssh://git@github.com/sonms/Velvet-Compose.git")
+        }
+    }
+
+    publishToMavenCentral()
+    signAllPublications()
 }
